baf321d1176e7b808f053595070e6697162218f2
Allure149 <types140@fahr-zur-hoelle.org>
2020-09-04 23:35:19 +0200

    new index.js

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 console.log("Welcome to LSS-Cockpit!");
 
-var lsscAppStore = document.createElement("div");
-lsscAppStore.setAttribute("id","lsscAppStore");
-lsscAppStore.innerText = "Tescht";
-document.getElementById("main_navbar").insertAfter(lsscAppStore);
+document.getElementById("main_navbar").style.cssText = "margin-bottom: 0px; border-bottom: 0px";
+    document.getElementById("map_outer").style.marginTop = "20px";
+    document.getElementById("missions_outer").style.marginTop = "20px";
 
-var lsscAppStoreLabel = document.createElement("div");
-lsscAppStoreLabel.setAttribute("id","lsscAppStoreLabel");
-lsscAppStoreLabel.innerHTML = `<div class="label label-default pull-right" style="margin: -2em 3em 0 0">LSS-Cockpit</div>`;
-document.getElementById("lsscAppStore").insertAfter(lsscAppStoreLabel);
+    var mainNavbar = document.getElementById("main_navbar");
 
-document.getElementById("lsscAppStoreLabel").addEventListener("click", function(){
-    var appStore = document.getElementById("lsscAppStore");
-    if(appStore.innerHeight > 0) appStore.slideUp();
-    else appStore.slideDown();
-});
+    var cssAppstore = document.createElement('link');
+    cssAppstore.setAttribute('rel', 'stylesheet');
+    cssAppstore.setAttribute('type', 'text/css');
+    cssAppstore.setAttribute('href', 'https://quizzical-curran-b45735.netlify.app/css/appstore.css');
+    document.querySelector("head").appendChild(cssAppstore);
+
+    var cssCards = document.createElement('link');
+    cssCards.setAttribute('rel', 'stylesheet');
+    cssCards.setAttribute('type', 'text/css');
+    cssCards.setAttribute('href', 'https://quizzical-curran-b45735.netlify.app/css/cards.css');
+    document.querySelector("head").appendChild(cssCards);
+
+    var lsscAppStore = document.createElement("div");
+    lsscAppStore.setAttribute("id","lsscAppStore");
+    lsscAppStore.innerText = "Tescht";
+    lsscAppStore.style.cssText = "visibility: hidden; height: 0px; margin-top: 0px";
+    lsscAppStore.style.width = mainNavbar.clientWidth+"px";
+    mainNavbar.insertAfter(lsscAppStore);
+
+    var lsscAppStoreLabel = document.createElement("div");
+    lsscAppStoreLabel.setAttribute("id","lsscAppStoreLabel");
+    lsscAppStoreLabel.style.margin = "-5px 2em 0 0";
+    lsscAppStoreLabel.classList.add("pull-right");
+    lsscAppStoreLabel.innerHTML = `<div class="label label-default">LSS-Cockpit</div>`;
+    document.getElementById("lsscAppStore").insertAfter(lsscAppStoreLabel);
+
+    document.getElementById("lsscAppStoreLabel").addEventListener("click", function(){
+        var appStore = document.getElementById("lsscAppStore");
+
+        if(appStore.style.visibility == "hidden"){
+            console.log((document.getElementById("row-main-template").clientHeight-20));
+            appStore.style.cssText = "visibility: visible; height: "+(window.innerHeight - document.getElementById("main_navbar").clientHeight - 20)+"px; margin-top: -21px";
+            appStore.style.width = mainNavbar.clientWidth+"px";
+        }
+        else{
+            appStore.style.cssText = "visibility: hidden; height: 0px; margin-top: 0px";
+        }
+    });
